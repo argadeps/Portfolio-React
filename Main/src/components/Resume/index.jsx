ae69e4bf5c9c3a82760002499612d7adec76dbbd
[Pral Argade] <[pralargade@outlook.com]>
2025-03-12 18:36:42 -0400

    code modifications

--- Main/src/components/Resume/index.jsx
+++ Main/src/components/Resume/index.jsx
@@ -3,28 +3,21 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://docs.google.com/document/d/1uO3nQs2FCY9WokBjRs-O_44NBBFNdtaH/edit?usp=sharing&ouid=104318783665957932651&rtpof=true&sd=true">resume</a>
         </p>
-        <h3>Front-end Proficiencies</h3>
+        <h3>Core Competencies</h3>
         <ul className="skills">
-          <li>HTML</li>
-          <li>CSS</li>
+          <li>Automated & Manual Testing</li>
+          <li>Health Level 7 (HL7)</li>
+          <li>Project Management</li>
+          <li>Waterfall & Agile Methodologies</li>
+          <li>Data Extraction & Analysis</li>
+          <li>Application Testing Lifecycle</li>
+          <li>Software Development Lifecycle</li>
+          <li>Defect Tracking Management</li>
           <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>responsive design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
         </ul>
-        <h3>Back-end Proficiencies</h3>
-        <ul className="skills">
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>PostgreSQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
+        
       </div>
     </section>
   );
